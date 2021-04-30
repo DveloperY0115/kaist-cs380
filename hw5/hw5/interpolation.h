ff62a81f58dc58b97d45ccc660224f88dd7fc272
Seungwoo Yoo <dreamy1534@gmail.com>
2021-04-30 21:13:43 +0900

    feat(main): Add function 'Linear' for RigTForm interpolation

--- hw5/hw5/interpolation.h
+++ hw5/hw5/interpolation.h
@@ -6,12 +6,12 @@
 namespace Interpolation {
 
 	//! Linear interpolation of two coordinate vectors
-	inline Cvec3 lerp(Cvec3 c0, Cvec3 c1, double alpha) {
+	inline Cvec3 lerp(const Cvec3& c0, const Cvec3& c1, const double& alpha) {
 		return c0 * (1 - alpha) + c1 * alpha;
 	}
 
 	//! Spherical interpolation of two quaternions
-	inline Quat slerp(Quat q0, Quat q1, double alpha) {
+	inline Quat slerp(const Quat& q0, const Quat& q1, const double& alpha) {
 		Quat base = q1 * inv(q0);
 
 		if (base(0) < 0) {
@@ -21,6 +21,14 @@ namespace Interpolation {
 		}
 
 		return pow(base, alpha) * q0;
+	}
+
+	//! Linear interpolation of two RigTForms
+	inline RigTForm Linear(const RigTForm& rbt0, const RigTForm& rbt1, const double& alpha) {
+		Cvec3 interTrans = lerp(rbt0.getTranslation(), rbt1.getTranslation(), alpha);
+		Quat interRot = slerp(rbt0.getRotation(), rbt1.getRotation(), alpha);
+
+		return RigTForm(interTrans, interRot);
 	}
 }
 #endif
