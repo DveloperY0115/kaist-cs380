6e06ebe029742f0cf7d1de0cec2c6e20a75a7822
Seungwoo Yoo <dreamy1534@gmail.com>
2021-05-10 20:38:48 +0900

    feat(main): Add new files for assignment 6

--- hw6/hw6/scenegraph.cpp
+++ hw6/hw6/scenegraph.cpp
@@ -5,87 +5,70 @@
 using namespace std;
 
 bool SgTransformNode::accept(SgNodeVisitor& visitor) {
-    if (!visitor.visit(*this))
-        return false;
-    for (int i = 0, n = children_.size(); i < n; ++i) {
-        if (!children_[i]->accept(visitor))
-            return false;
-    }
-    return visitor.postVisit(*this);
+  if (!visitor.visit(*this))
+    return false;
+  for (int i = 0, n = children_.size(); i < n; ++i) {
+    if (!children_[i]->accept(visitor))
+      return false;
+  }
+  return visitor.postVisit(*this);
 }
 
 void SgTransformNode::addChild(shared_ptr<SgNode> child) {
-    children_.push_back(child);
+  children_.push_back(child);
 }
 
 void SgTransformNode::removeChild(shared_ptr<SgNode> child) {
-    children_.erase(find(children_.begin(), children_.end(), child));
+  children_.erase(find(children_.begin(), children_.end(), child));
 }
 
 bool SgShapeNode::accept(SgNodeVisitor& visitor) {
-    if (!visitor.visit(*this))
-        return false;
-    return visitor.postVisit(*this);
+  if (!visitor.visit(*this))
+    return false;
+  return visitor.postVisit(*this);
 }
 
 class RbtAccumVisitor : public SgNodeVisitor {
 protected:
-    vector<RigTForm> rbtStack_;
-    SgTransformNode& target_;
-    bool found_;
-
+  vector<RigTForm> rbtStack_;
+  SgTransformNode& target_;
+  bool found_;
 public:
-    RbtAccumVisitor(SgTransformNode& target)
-        : target_(target)
-        , found_(false) 
-    {
-        // push identity
-        rbtStack_.push_back(RigTForm());
+  RbtAccumVisitor(SgTransformNode& target)
+    : target_(target)
+    , found_(false) {}
+
+  const RigTForm getAccumulatedRbt(int offsetFromStackTop = 0) {
+    if (!found_)
+      throw runtime_error("RbtAccumVisitor target never reached");
+    return rbtStack_[rbtStack_.size()-1-offsetFromStackTop];
+  }
+
+  virtual bool visit(SgTransformNode& node) {
+    if (rbtStack_.empty())
+      rbtStack_.push_back(RigTForm());
+    else
+      rbtStack_.push_back(rbtStack_.back() * node.getRbt());
+
+    if (target_ == node) {
+      found_ = true;
+      return false;
     }
+    return true;
+  }
 
-    const RigTForm getAccumulatedRbt(int offsetFromStackTop = 0) {
-        int num_popped = 0;
-
-        while (!rbtStack_.empty() && num_popped < offsetFromStackTop) {
-            rbtStack_.pop_back();
-            num_popped++;
-        }
-
-        return rbtStack_.back();
-    }
-
-    virtual bool visit(SgTransformNode& node) {
-
-        rbtStack_.push_back(rbtStack_.back() * node.getRbt());
-
-        if (target_ == node) {
-            found_ = true;
-            // if target found, stop searching
-            return false;
-        }
-
-        // otherwise, keep searching
-        return true;
-    }
-
-    virtual bool postVisit(SgTransformNode& node) {
-        if (!found_) {
-            // discard all nodes in the middle if target is not found
-            rbtStack_.pop_back();
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
+  virtual bool postVisit(SgTransformNode& node) {
+    rbtStack_.pop_back();
+    return true;
+  }
 };
 
 RigTForm getPathAccumRbt(
-    shared_ptr<SgTransformNode> source,
-    shared_ptr<SgTransformNode> destination,
-    int offsetFromDestination) {
+  shared_ptr<SgTransformNode> source,
+  shared_ptr<SgTransformNode> destination,
+  int offsetFromDestination) {
 
-    RbtAccumVisitor accum(*destination);
-    source->accept(accum);
-    return accum.getAccumulatedRbt(offsetFromDestination);
+  RbtAccumVisitor accum(*destination);
+  source->accept(accum);
+  return accum.getAccumulatedRbt(offsetFromDestination);
 }
